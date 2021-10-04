df58084e3f7ed13ce704ae6c5260ad54051d80b8
Cameron <cameronnagel8000@gmail.com>
2021-10-04 16:25:43 +0200

    Added "ListBox" & "Button" to the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbDisplay = new System.Windows.Forms.ListBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.FormattingEnabled = true;
+            this.lbDisplay.Location = new System.Drawing.Point(216, 31);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(281, 160);
+            this.lbDisplay.TabIndex = 0;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(304, 239);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(110, 34);
+            this.btnDisplay.TabIndex = 1;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lbDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbDisplay;
+        private System.Windows.Forms.Button btnDisplay;
     }
 }
 
